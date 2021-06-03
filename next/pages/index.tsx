ba3cdc9fbc12a2c89615537bf554732d14f7f790
Kyungseo-Park <troublesome.dev@gmail.com>
2021-06-03 23:05:23 +0900

    Run pipeline

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="CI 테스트">
+  <Layout title="음 ">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
