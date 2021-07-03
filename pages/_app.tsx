7e8e60201bd2b203c0dcb901b7a0387c3d9de878
kyungseo.park <troublesome.dev@gmail.com>
2021-07-03 21:46:03 +0900

    :package: add styled-components theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import withReduxSaga from "next-redux-saga";
 import wrapper from "@/store/configureStore";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "@/styles/theme";
+import GlobalStyle from "@/styles/GlobalStyle";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Component {...pageProps} />
-      <div id="root-modal" />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+        <div id="root-modal" />
+      </ThemeProvider>
     </>
   );
 };
---
+++ styles/GlobalStyle.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle, css } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyle = css`
+  ${reset};
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Noto Sans, Noto Sans KR";
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+    ${globalStyle}
+`;
+
+export default GlobalStyle;
