2a64d38c0b2ccedf2819619a19fe81c0d12ad3e1
Kyungseo-Park <troublesome.dev@gmail.com>
2021-09-15 01:45:31 +0900

    add product upload page

---
+++ components/Product/Upload/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProductUploadContainer = styled.div``;
+const ProductUpload: React.FC = () => {
+  return (
+    <ProductUploadContainer>
+      <span>업ㄹ드</span>
+    </ProductUploadContainer>
+  );
+};
+
+export default ProductUpload;
